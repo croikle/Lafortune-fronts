e97e59179314b299a0b9ce7126b42ab2c1259c94
Peter McLarnan <croikle@gmail.com>
2012-07-13 20:34:32 -0400

    simplify display while searching

--- integrated_find_c.m
+++ integrated_find_c.m
@@ -3,17 +3,18 @@ function c = integrated_find_c(eps,h,Z,sigma,w_star)
   c_high = 10;
   init_pt = 1e-4;
 
-  precision = 1e-7;
+  precision = 1e-5;
 
   while c_high - c_low > precision
     test_c = (c_low + c_high)/2;
     [x u v] = integrated_solve(test_c,eps,h,Z,sigma,w_star,10,0.01,[1-init_pt,1-init_pt]);
     terminus = u(end);
     if terminus < 0
-      c_low = test_c
+      c_low = test_c;
     else
-      c_high = test_c
+      c_high = test_c;
     end
+    disp(sprintf('%f ... %f', c_low, c_high));
   end
   c = c_low;
 end
