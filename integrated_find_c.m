f8d4512053fa493de183976064271650ac09ea64
Peter McLarnan <croikle@gmail.com>
2012-07-17 15:34:28 -0400

    use solution structures and deval

--- integrated_find_c.m
+++ integrated_find_c.m
@@ -1,14 +1,15 @@
-function [x, u, v] = integrated_find_c(eps,h,Z,sigma,w_star)
+function [c, sol] = integrated_find_c(eps,h,Z,sigma,w_star)
   c_low = 1;
   c_high = 10;
   init_pt = 1e-9;
+  final_time = 10;
 
   precision = 1e-5;
 
   while c_high - c_low > precision
     test_c = (c_low + c_high)/2;
-    [x u v] = integrated_solve(test_c,eps,h,Z,sigma,w_star,10,0.01,[1-init_pt,1-init_pt]);
-    terminus = u(end);
+    sol = integrated_solve(test_c,eps,h,Z,sigma,w_star,final_time,[1-init_pt,1-init_pt]);
+    terminus = deval(sol,final_time,1);
     if terminus < 0
       c_low = test_c;
     else
--- integrated_solve.m
+++ integrated_solve.m
@@ -1,15 +1,14 @@
 % depends on integrated_ode_eps0.m, integrated_ode.m
-function [X, U, V] = integrated_solve(c,eps,h,Z,sigma,w_star,L,dx,initial)
+function sol = integrated_solve(c,eps,h,Z,sigma,w_star,L,initial)
   options = odeset('RelTol',1e-9,'AbsTol',1e-9);
 
-  x_values = [dx:dx:L];
+  x_values = [0,L];
   if eps == 0
     ode = integrated_ode_eps0(c,h,Z,sigma,w_star);
-    [X, Y] = ode45(ode, x_values, initial, options);
   else
     ode = integrated_ode(c,eps,h,Z,sigma,w_star);
-    [X, Y] = ode45(ode, x_values, [initial initial(2)], options);
+    initial = [initial initial(2)];
+    % need initial value for z too -- take V init as hack
   end
-  U = Y(:,1);
-  V = Y(:,2);
+  sol = ode45(ode, x_values, initial, options);
 end
