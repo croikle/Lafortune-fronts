f7a98e3ce16bc5ce1f95f8c0f342b04189c8b643
Peter McLarnan <croikle@gmail.com>
2013-03-04 11:19:34 -0500

    adjoint is -A', not positive

--- adjoint_ode.m
+++ adjoint_ode.m
@@ -3,7 +3,7 @@ function result = A_ode(c,lambda,h,Z,sigma,w_star)
   our_A = A(c,lambda,h,Z,sigma,w_star);
 
   function dx = ode(t,x)
-    dx = our_A(x).' * x;
+    dx = -our_A(x).' * x;
   end
 
   result = @ode;
