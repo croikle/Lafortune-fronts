c71b56cefdac4ebb34182121a3114ee82152f2b3
Peter McLarnan <croikle@gmail.com>
2013-06-07 13:31:49 -0400

    return front from evans, take ends and midpoint as arguments

--- evans.m
+++ evans.m
@@ -2,7 +2,7 @@
 % recursive depends: A.m, fu.m, fv.m
 % also the whole integrated_* tree
 % specialized to epsilon = 0 for the moment
-function result = evans(eps,h,Z,sigma,w_star)
+function [result, sol] = evans(eps,h,Z,sigma,w_star)
 % returns a function of lambda
   [c, front, sol] = integrated_find_c(eps,h,Z,sigma,w_star);
   % where is the front basically done
@@ -13,30 +13,33 @@ function result = evans(eps,h,Z,sigma,w_star)
 
   % meet in the middle
   % later: extend before 0, shift to put middle at 0
-  t1_values = [endval, endval/2];
-  t2_values = [0, endval/2];
-  length = endval/2;
+  % t1_values = [endval, 0];
+  % t2_values = [0, endval];
+  % length = endval/2;
 
-  function [value,sol1,sol2] = compute(lambda,rescale,varargin)
+  function [value,sol1,sol2] = compute(lambda,rescale,left,right,mid,varargin)
     eigenvalue = -c/2 - sqrt(c^2 + 4*lambda)/2;
-    scale = exp(length*eigenvalue);
 
     ode1 = A_ode(c,front,lambda,h,Z,sigma,w_star);
     vector1 = rescale * [1, -(c + sqrt(c^2 + 4*lambda))/2, 0];
     % this is the stable eigenvector of the limiting matrix at 0,0
-    initial1 = scale * vector1;
+    scale1 = exp((right-mid)*eigenvalue);
+    initial1 = scale1 * vector1;
+    t1_values = [right, mid];
     sol1 = ode45(ode1, t1_values, initial1, options);
 
     ode2 = adjoint_ode(c,front,lambda,h,Z,sigma,w_star);
     vector2 = [1, -1/2*(c + sqrt(c^2 + 4*lambda))/lambda, (c^2 + sqrt(c^2 + 4*lambda)*c)*exp(Z)/((c^2*lambda + sqrt(c^2 + 4*lambda)*c*lambda + 2*lambda^2)*exp(Z*h) + 2*lambda*exp(Z))];
     % this is the stable eigenvector of the -transposed limiting matrix at 1,1
     v2_normalized = vector2 / dot(vector1,vector2);
-    initial2 = scale * v2_normalized;
+    scale2 = exp((mid-left)*eigenvalue);
+    initial2 = scale2 * v2_normalized;
+    t2_values = [left, mid];
     sol2 = ode45(ode2, t2_values, initial2, options);
 
     % for debugging. make sure these solutions don't go weird
     % pass an extra argument to show plots.
-    debug = (nargin > 2);
+    debug = (nargin > 5);
     if(debug)
       figure(1);
       plot(sol1.x,sol1.y);
