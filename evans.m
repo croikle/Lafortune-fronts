f12a5d9d03886c946379c6630ed77b936dca1233
Peter McLarnan <croikle@gmail.com>
2013-06-21 16:51:02 -0400

    use eps parameter, rewrite eps_nonzero

--- evans.m
+++ evans.m
@@ -21,7 +21,7 @@ function [result, sol] = evans(eps,h,Z,sigma,w_star)
   function [value,sol1,sol2] = compute_eps0(lambda, options, debug)
     eigenvalue = -c/2 - sqrt(c^2 + 4*lambda)/2;
 
-    ode1 = A_ode(c,front,lambda,h,Z,sigma,w_star);
+    ode1 = A_ode(eps,c,front,lambda,h,Z,sigma,w_star);
     vector1 = [1, -(c + sqrt(c^2 + 4*lambda))/2, 0];
     % this is the stable eigenvector of the limiting matrix at 0,0
     scale1 = exp((right-mid)*eigenvalue);
@@ -29,7 +29,7 @@ function [result, sol] = evans(eps,h,Z,sigma,w_star)
     t1_values = [right, mid];
     sol1 = ode45(ode1, t1_values, initial1, options);
 
-    ode2 = adjoint_ode(c,front,lambda,h,Z,sigma,w_star);
+    ode2 = adjoint_ode(eps,c,front,lambda,h,Z,sigma,w_star);
     vector2 = [1, -1/2*(c + sqrt(c^2 + 4*lambda))/lambda, (c^2 + sqrt(c^2 + 4*lambda)*c)*exp(Z)/((c^2*lambda + sqrt(c^2 + 4*lambda)*c*lambda + 2*lambda^2)*exp(Z*h) + 2*lambda*exp(Z))];
     % this is the stable eigenvector of the -transposed limiting matrix at 1,1
     v2_normalized = vector2 / dot(vector1,vector2);
@@ -52,41 +52,59 @@ function [result, sol] = evans(eps,h,Z,sigma,w_star)
 
   function [value,sol1,sol2,sol3,sol4] = compute_eps_nonzero(lambda, options, debug)
     % negative real part eigenvalues of the limit at 0,0 (+infinity)
-    eigenvalue1 = -(c + sqrt(c^2 + 4*eps*lambda))/(2*eps);
-    eigenvalue2 = -c/2 - sqrt(c^2 + 4*lambda)/2;
+    lambda1 = -(c + sqrt(c^2 + 4*eps*lambda))/(2*eps);
+    lambda2 = -c/2 - sqrt(c^2 + 4*lambda)/2;
+    % positive real part eigenvalues of the limit at 0,0 (+infinity)
+    lambda3 = -(c - sqrt(c^2 + 4*eps*lambda))/(2*eps);
+    lambda4 = -c/2 + sqrt(c^2 + 4*lambda)/2;
+
     % positive real part eigenvalues of the limit at 1,1 (-infinity)
-    eigenvalue3 = -1/2*(c*exp(Z*h - Z) - sqrt((c^2 + 4*eps*lambda)*exp(2*Z*h - 2*Z) + 4*eps*exp(Z*h - Z)))*exp(-Z*h + Z)/eps;
-    eigenvalue4 = -c/2 + sqrt(c^2 + 4*lambda)/2;
+    nu1 = -1/2*(c*exp(Z*h - Z) - sqrt((c^2 + 4*eps*lambda)*exp(2*Z*h - 2*Z) + 4*eps*exp(Z*h - Z)))*exp(-Z*h + Z)/eps;
+    nu2 = -c/2 + sqrt(c^2 + 4*lambda)/2;
+    % negative real part eigenvalues of the limit at 1,1 (-infinity)
+    nu3 = -1/2*(c*exp(Z*h - Z) + sqrt((c^2 + 4*eps*lambda)*exp(2*Z*h - 2*Z) + 4*eps*exp(Z*h - Z)))*exp(-Z*h + Z)/eps;
+    nu4 = -c/2 - sqrt(c^2 + 4*lambda)/2;
 
-    eigenvector1 = [0,0,1,eigenvalue1];
-    eigenvector2 = [1,eigenvalue2,0,0];
-    eigenvector3 = [1, ((sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c*eps - sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c)*exp(3/2*Z*h) - (c^2*eps - c^2 - 2*eps*lambda)*exp(2*Z*h) + 2*eps*exp(Z*h + Z))/(sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*eps*exp(3/2*Z*h) + (2*c*eps^2 - c*eps)*exp(2*Z*h)), -1/2*(c^2*eps*exp(3*Z*h - Z) - c^2*exp(3*Z*h - Z) - ((sqrt(c^2*exp(2*Z*h - 2*Z) + 4*eps*lambda*exp(2*Z*h - 2*Z) + 4*eps*exp(Z*h - Z))*c + 2)*eps - sqrt(c^2*exp(2*Z*h - 2*Z) + 4*eps*lambda*exp(2*Z*h - 2*Z) + 4*eps*exp(Z*h - Z))*c)*exp(2*Z*h) + 2*(eps^2*exp(3*Z*h - Z) - eps*exp(3*Z*h - Z))*lambda)*exp(-2*Z*h)/eps^2, 1/2*(2*c*eps^2 + (sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*exp(-1/2*Z*h) - 3*c)*eps - (sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c^2*eps*exp(-Z*h - Z) - sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c^2*exp(-Z*h - Z) + (sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*eps^2*exp(-Z*h - Z) - sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*eps*exp(-Z*h - Z))*lambda)*exp(3/2*Z*h) + (c^3*eps*exp(-Z*h - Z) - c^3*exp(-Z*h - Z) + 3*(c*eps^2*exp(-Z*h - Z) - c*eps*exp(-Z*h - Z))*lambda)*exp(2*Z*h))/eps^3];
+    v1 = [0,0,1,lambda1];
+    v2 = [1,lambda2,0,0];
+    v3 = [0,0,1,lambda3];
+    v4 = [1,lambda4,0,0];
+
+    w1 = [1, ((sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c*eps - sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c)*exp(3/2*Z*h) - (c^2*eps - c^2 - 2*eps*lambda)*exp(2*Z*h) + 2*eps*exp(Z*h + Z))/(sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*eps*exp(3/2*Z*h) + (2*c*eps^2 - c*eps)*exp(2*Z*h)), -1/2*(c^2*eps*exp(3*Z*h - Z) - c^2*exp(3*Z*h - Z) - ((sqrt(c^2*exp(2*Z*h - 2*Z) + 4*eps*lambda*exp(2*Z*h - 2*Z) + 4*eps*exp(Z*h - Z))*c + 2)*eps - sqrt(c^2*exp(2*Z*h - 2*Z) + 4*eps*lambda*exp(2*Z*h - 2*Z) + 4*eps*exp(Z*h - Z))*c)*exp(2*Z*h) + 2*(eps^2*exp(3*Z*h - Z) - eps*exp(3*Z*h - Z))*lambda)*exp(-2*Z*h)/eps^2, 1/2*(2*c*eps^2 + (sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*exp(-1/2*Z*h) - 3*c)*eps - (sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c^2*eps*exp(-Z*h - Z) - sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c^2*exp(-Z*h - Z) + (sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*eps^2*exp(-Z*h - Z) - sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*eps*exp(-Z*h - Z))*lambda)*exp(3/2*Z*h) + (c^3*eps*exp(-Z*h - Z) - c^3*exp(-Z*h - Z) + 3*(c*eps^2*exp(-Z*h - Z) - c*eps*exp(-Z*h - Z))*lambda)*exp(2*Z*h))/eps^3];
 % yes, it's ridiculous. This is what I got out of Sage; it may be simplifiable.
-    eigenvector4 = [1,eigenvalue4,0,0];
+    w2 = [1,nu2,0,0];
+    w3 = [1, ((sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c*eps - sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c)*exp(3/2*Z*h) + (c^2*eps - c^2 - 2*eps*lambda)*exp(2*Z*h) - 2*eps*exp(Z*h + Z))/(sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*eps*exp(3/2*Z*h) - (2*c*eps^2 - c*eps)*exp(2*Z*h)), -1/2*(c^2*eps*exp(3*Z*h - Z) - c^2*exp(3*Z*h - Z) + ((sqrt(c^2*exp(2*Z*h - 2*Z) + 4*eps*lambda*exp(2*Z*h - 2*Z) + 4*eps*exp(Z*h - Z))*c - 2)*eps - sqrt(c^2*exp(2*Z*h - 2*Z) + 4*eps*lambda*exp(2*Z*h - 2*Z) + 4*eps*exp(Z*h - Z))*c)*exp(2*Z*h) + 2*(eps^2*exp(3*Z*h - Z) - eps*exp(3*Z*h - Z))*lambda)*exp(-2*Z*h)/eps^2, 1/2*(2*c*eps^2 - (sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*exp(-1/2*Z*h) + 3*c)*eps + (sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c^2*eps*exp(-Z*h - Z) - sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*c^2*exp(-Z*h - Z) + (sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*eps^2*exp(-Z*h - Z) - sqrt(c^2*exp(Z*h) + 4*eps*lambda*exp(Z*h) + 4*eps*exp(Z))*eps*exp(-Z*h - Z))*lambda)*exp(3/2*Z*h) + (c^3*eps*exp(-Z*h - Z) - c^3*exp(-Z*h - Z) + 3*(c*eps^2*exp(-Z*h - Z) - c*eps*exp(-Z*h - Z))*lambda)*exp(2*Z*h))/eps^3];
+    w4 = [1,nu4,0,0];
 
-    ode = A_ode(c,front,lambda,h,Z,sigma,w_star);
+    ode = A_ode(eps,c,front,lambda,h,Z,sigma,w_star);
+    t_values_right = [right, mid];
+    t_values_left = [left, mid];
 
-    scale1 = exp((right-mid)*eigenvalue1);
-    initial1 = scale1 * eigenvector1;
-    t1_values = [right, mid];
-    sol1 = ode45(ode, t1_values, initial1, options);
+    function [sol1,sol2] = integrate_wronsk(eigenvalues, eigenvectors, t_values)
+      len = t_values(2) - t_values(1);
+      % not taking the absolute value, since we're not doing the adjoint.
+      % correct?
 
-    scale2 = exp((right-mid)*eigenvalue2);
-    initial2 = scale2 * eigenvector2;
-    t2_values = [right, mid];
-    sol2 = ode45(ode, t2_values, initial2, options);
-% any normalizing the scale of the eigenvectors?
-% last time we ensured v1 . v2 = 1
+      for i = 1:4
+        scale = exp(len*eigenvalues(i));
+        initial = scale * eigenvectors{i};
+        % any normalizing the scale of the eigenvectors?
+        % last time we ensured v1 . v2 = 1
+        sols(i) = ode45(ode, t_values, initial, options);
+      end
 
-    scale3 = exp((mid-left)*eigenvalue3);
-    initial3 = scale3 * eigenvector3;
-    t3_values = [left, mid];
-    sol3 = ode45(ode3, t3_values, initial3, options);
+      wronsk_initial = det([sols(1).y(:,1), sols(2).y(:,1), sols(3).y(:,1), sols(4).y(:,1)]);
+      wronsk_final = det([sols(1).y(:,end), sols(2).y(:,end), sols(3).y(:,end), sols(4).y(:,end)])
+      traceA = -c*(1 + 1/eps); % luckily independent of \xi
+      wronsk_expected = wronsk_initial * exp(len*traceA)
+      err = abs((wronsk_final - wronsk_expected)/wronsk_expected)
 
-    scale4 = exp((mid-left)*eigenvalue4);
-    initial4 = scale4 * eigenvector4;
-    t4_values = [left, mid];
-    sol4 = ode45(ode4, t4_values, initial4, options);
+      sol1 = sols(1);
+      sol2 = sols(2);
+    end
+
+    [sol1, sol2] = integrate_wronsk([lambda1, lambda2, lambda3, lambda4], {v1, v2, v3, v4}, t_values_right);
+    [sol3, sol4] = integrate_wronsk([nu1, nu2, nu3, nu4], {w1, w2, w3, w4}, t_values_left);
 
     % for debugging. make sure these solutions don't go weird
     % pass an extra argument to show plots.
