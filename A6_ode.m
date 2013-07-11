c467fa64ff36c18690b19f2032521637bf246ef4
Peter McLarnan <croikle@gmail.com>
2013-07-11 18:37:39 -0400

    rescaled using eigenvalue (B.12)

--- A6_ode.m
+++ A6_ode.m
@@ -1,11 +1,12 @@
-function result = A6_ode(eps,c,front,lambda,h,Z,sigma,w_star)
+function result = A6_ode(eps,c,front,eigenvalue,lambda,h,Z,sigma,w_star)
   our_A = A6(eps,c,lambda,h,Z,sigma,w_star);
+  rescaled_A = @(x) our_A(x) - eigenvalue * eye(6);
 
   function dp = ode(t,p)
     % find the front at time t
     x = front(t);
     % use A at that x value
-    dp = our_A(x) * p;
+    dp = rescaled_A(x) * p;
   end
 
   result = @ode;
